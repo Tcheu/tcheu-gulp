b2480f87fa7c0fa22d87f151a3e8a0353ba55da7
Quentin Delcourt <kant312@gmail.com>
2016-05-12 15:17:46 +0200

    v3

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,9 @@ var config = {
     'browsersync': {
         'open': false,
         'port': '3000',
-        'server': {
-            'baseDir': 'public'
-        },
+        'ghostMode': false,
+        'proxy': 'tcheu-gulp.dev',
+        'online': true,
         'notify': true
     },
     // Where to store the build files
@@ -33,25 +33,52 @@ var config = {
     // These are the HTML or PHP files to watch
     'html': {
         'src': [
-            'craft/templates/**/*.html'
+            'craft/templates/**/*.twig',
+            'craft/config/**/*.php'
         ]
     },
     // Images folders for image optimisation
     'img': {
-        'src': 'public/assets/img/**/*.+(png|jpg|jpeg|gif|svg|ico)',
+        'src': 'resources/assets/img/**/*.+(png|jpg|jpeg|gif|svg|ico)',
         'dist': 'public/dist/img/'
+    },
+    // Copy vendors assets
+    'vendors': {
+        'src': 'resources/assets/vendors/**/*',
+        'dist': 'public/dist/vendors/'
     },
     // JavaScript files
     'js': {
-        'src': 'public/assets/js/',
+        'src': 'public/assets/js/src/',
         'dist': 'public/dist/js/',
-        'bundleName': 'main.js'
+        'bundleName': 'main.bundle.js'
+    },
+    //JSHint configuration
+    'jshint': {
+        'esnext': true,
+        'laxbreak': true,
+        'laxcomma': true
     },
     // Source SVG files to create a spritemap
     'svg': {
-        'src': 'public/assets/img/svg/sprite_sources/',
-        'dist': 'public/img/svg/',
+        'src': 'public/dist/img/svg/sprite_sources/',
+        'dist': 'public/dist/img/svg/',
         'spriteFile': 'svgsprite.svg'
+    },
+    //Environments
+    'env': {
+        'staging': {
+            'host': 'your-staging-server.com',
+            'deployScript': '/path/to/your/deploy/script.sh',
+            'syncSrc': '/path/to/upload/folder',
+            'syncDest': '/path/to/local/folder'
+        },
+        'production': {
+            'host': 'your-production-server.com',
+            'deployScript': '/path/to/your/deploy/script.sh',
+            'syncSrc': '/path/to/upload/folder',
+            'syncDest': '/path/to/local/folder'
+        }
     }
 };
 
@@ -70,7 +97,8 @@ var gulp				= require('gulp'),
     gutil				= require('gulp-util'),
     imagemin			= require('gulp-imagemin'),
     jshint				= require('gulp-jshint'),
-    minifycss			= require('gulp-minify-css'),
+    jspm				= require('gulp-jspm'),
+    cleanCSS	    	= require('gulp-clean-css'),
     notify				= require('gulp-notify'),
     path				= require('path'),
     plumber				= require('gulp-plumber'),
@@ -79,7 +107,7 @@ var gulp				= require('gulp'),
     rev 				= require('gulp-rev'),
     sass				= require('gulp-sass'),
     size 				= require('gulp-size'),
-    stripdebug			= require('gulp-strip-debug'),
+    strstripDebug		= require('gulp-strip-debug'),
     svgstore			= require('gulp-svgstore'),
     uglify				= require('gulp-uglify');
 
@@ -117,36 +145,34 @@ gulp.task('scss', function() {
         .pipe(sass({ style: 'expanded' }))
         .pipe(gulp.dest( config.scss.dist ))
         .pipe(autoprefixer( config.autoprefixer ))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss())
-        .pipe(gulp.dest( config.scss.dist ))
         .pipe(browsersync.reload({stream: true}))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS({compatibility: 'ie9'}))
+        .pipe(gulp.dest( config.scss.dist ))
         .pipe(notify({ message: 'Styles task done' }))
         .pipe(size({ title: 'scss' }));
 });
 
 // Lint JS task
-gulp.task('jslint', function() {
-    return gulp.src( config.js.src + '**/*.js' )
-        .pipe(plumber({ errorHandler: onError }))
-        .pipe(jshint())
+gulp.task('jshint', function() {
+    var paths = [config.js.src + '**/*.js', '!' + config.js.src + 'polyfills/**', '!' + config.js.src + 'libs/**/*.js'];
+    return gulp.src( paths )
+        .pipe(jshint( config.jshint ))
         .pipe(jshint.reporter('default'))
-        .pipe(jshint.reporter('fail'))
-        .pipe(notify({ message: 'JS lint task done' }));
+        .pipe(jshint.reporter('fail'));
 });
 
-// Concatenate and Minify JS task
-gulp.task('scripts', function() {
-    return gulp.src( config.js.src + '**/*.js' )
-        .pipe(concat( config.js.bundleName ))
-        .pipe(gulp.dest( config.js.dist ))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(stripdebug())
+// JSPM
+gulp.task('jspm', ['jshint'], function() {
+    del.sync(config.js.dist + '**/*');
+    return gulp.src(config.js.src + 'main.js')
+        .pipe(plumber({ errorHandler: onError }))
+        .pipe(jspm())
+        .pipe(gulp.dest('public/dist/js/'))
         .pipe(uglify())
-        .pipe(gulp.dest( config.js.dist ))
-        .pipe(browsersync.reload({ stream:true }))
-        .pipe(notify({ message: 'Scripts task done' }))
-        .pipe(size({ title: 'scripts' }));
+        .pipe(strstripDebug())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('public/dist/js/'));
 });
 
 // Optimize Images task
@@ -160,6 +186,12 @@ gulp.task('optimize-img', function() {
         .pipe(notify({ message: 'Images task done', onLast: true }));
 });
 
+gulp.task('copy-vendors-assets',function(){
+    return gulp.src( config.vendors.src )
+        .pipe(gulp.dest( config.vendors.dist ))
+        .pipe(notify({ message: 'Vendors assets copied', onLast: true }));
+});
+
 // Make svg sprite
 gulp.task('svgsprite', ['optimize-img'], function() {
     return gulp.src( config.svg.src + '/**/' + '*.svg' )
@@ -169,8 +201,57 @@ gulp.task('svgsprite', ['optimize-img'], function() {
         .pipe(notify({ message: 'SVG sprite done' }));
 });
 
+//Update the database
+gulp.task('db', function(cb) {
+    var command = 'cd ../_scripts/ && ./db-update.sh';
+
+    exec(command, {maxBuffer: 1024 * 20000}, function(err, stdout, stderr) {
+        cb(err);
+    });
+});
+
+//Deploy staging
+gulp.task('staging', function(cb) {
+    var command = "/usr/bin/ssh " + config.env.staging.host + " '" + config.env.staging.deployScript + " staging'";
+
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+        cb(err);
+    });
+});
+
+//Deploy production
+gulp.task('production', function(cb) {
+    var command = "/usr/bin/ssh " + config.env.production.host + " '" + config.env.production.deployScript + " staging'";
+
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+        cb(err);
+    });
+});
+
+//Sync assets
+gulp.task('sync-staging', function(cb) {
+    var command = "/usr/bin/scp -r " + config.env.staging.host + ":" + config.env.staging.syncSrc + " " + config.env.staging.syncDest;
+
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+        cb(err);
+    });
+});
+
+//Sync assets from PROD
+gulp.task('sync-prod', function(cb) {
+    var command = "/usr/bin/scp -r " + config.env.production.host + ":" + config.env.production.syncSrc + " " + config.env.production.syncDest;
+
+    exec(command, function(err, stdout, stderr) {
+        console.log(stdout);
+        cb(err);
+    });
+});
+
 // Cache busting
-gulp.task('cachebust', ['scss', 'scripts'], function() {
+gulp.task('cachebust', ['scss', 'jspm'], function() {
     return gulp.src( config.build.dir + '/**/*.{css,js}' )
         .pipe(rev())
         .pipe(gulp.dest( config.build.dir ))
@@ -189,10 +270,11 @@ gulp.task('clean', function() {
 // Watch task
 gulp.task('watch', ['browser-sync'], function () {
     gulp.watch(config.scss.src + '**/*' , ['scss']);
-    gulp.watch(config.js.src + '**/*' , ['jslint', 'scripts']);
+    gulp.watch(config.js.src + '**/*' , ['jspm', 'browsersync-reload']);
     gulp.watch(config.html.src, ['browsersync-reload']);
 });
 
 // Tasks
-gulp.task('default', ['clean', 'img', 'scss', 'jslint', 'scripts', 'browsersync-reload', 'cachebust']);
+gulp.task('default', ['clean', 'img', 'copy-vendors-assets', 'scss', 'jspm', 'cachebust']);
+gulp.task('all', ['db', 'sync', 'clean', 'img', 'copy-vendors-assets', 'scss', 'jspm', 'cachebust']);
 gulp.task('img', ['optimize-img', 'svgsprite']);
